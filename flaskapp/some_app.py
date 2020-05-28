74a4c49892e4e90d9d957630e68d20a87b66a0ee
nf2g <nf2g.2000@gmail.com>
2020-05-28 18:11:19 +0700

    new

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -34,18 +34,11 @@ if __name__ == "__main__":
 # наша новая функция сайта
 # @app.route("/data_to")s
 # def data_to():
-    #создаем переменные с данными для передачи в шаблон
-    #some_pars = {'user':'Ivan','color':'red'}
-    #some_str = 'Hello my dear friends!'
-    #some_value = 10
-    #передаем данные в шаблон и вызываем его
-    #return render_template('simple.html',some_str = some_str,
-    #some_value = some_value,some_pars=some_pars)
 #     return render_template('simple.html')
 
 
 app.config['RECAPTCHA_USE_SSL'] = False
-app.config['RECAPTCHA_PUBLIC_KEY'] = '66LfxOv0UAAAAAN0Vgj9P3Qv98lGGKQoQYuh8XVL9'
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LfxOv0UAAAAAN0Vgj9P3Qv98lGGKQoQYuh8XVL9'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LfxOv0UAAAAACJfT_QmP-51aPLiBPGdRO6Nf6ie'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
 
