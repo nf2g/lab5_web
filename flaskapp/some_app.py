bad8657bad281a79bdecbfa62e89a1f4480bf999
nf2g <nf2g.2000@gmail.com>
2020-05-30 03:36:56 +0700

    #

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -95,9 +95,9 @@ def net():
             neurodic[elem[0][1]] = elem[0][2]
         # сохраняем загруженный файл
         #////////////////////////////////
-        form.upload.data = znak(form.upload.data)
+        #form.upload.data = znak(form.upload.data)
         #/////////////////////////////////
-        #form.upload.data.save(filename)
+        form.upload.data.save(filename)
         
     # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
     # сети если был нажат сабмит, либо передадим falsy значения
